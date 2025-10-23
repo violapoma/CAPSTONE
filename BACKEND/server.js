cc01e3aa57fd92b83b84114e667d1604b84f5410
violapoma <violapoma@gmail.com>
2025-10-23 18:32:08 +0200

    fix deploy
    #6.3

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -27,6 +27,7 @@ import passport from "passport";
 const server = express();
 const port = process.env.PORT;
 
+console.log('CORS ORIGIN: ', process.env.FRONTEND_HOST); 
 const corsOptions = {
   origin: process.env.FRONTEND_HOST,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
